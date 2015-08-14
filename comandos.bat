719fd8d325b4558c88bf2d1ce4e23ee3fdaf1398
David Asensio <dasensio13@gmail.com>
2015-08-14 23:46:42 +0200

    Soporte HTTPS

---
+++ comandos.bat
@@ -0,0 +1,5 @@
+mkdir cert
+cd cert
+"C:\Program Files\OpenSSL-Win32\bin\openssl" genrsa -out quiz-2015-key.pem 2048
+"C:\Program Files\OpenSSL-Win32\bin\openssl" req -new -sha256 -key quiz-2015-key.pem -out quiz-2015-csr.pem
+"C:\Program Files\OpenSSL-Win32\bin\openssl" x509 -req -in quiz-2015-csr.pem -signkey quiz-2015-key.pem -out quiz-2015-cert.pem
